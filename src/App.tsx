39a4e0fdfe4481fc001c4a8591cf9c92d15c8b80
jordansbenjamin <jordansbdev@gmail.com>
2026-05-22 21:40:16 +1000

    feat: added styles for app and main containers

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { useReducer } from "react"
 import Header from "./components/layout/Header"
 import StepView from "./components/layout/StepView"
 import { initialState, reducer } from "./state/reducer"
+import styles from "./App.module.css"
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
-    <div className="app">
+    <div className={styles.app}>
       <Header />
-      <main className="main">
+      <main className={styles.main}>
         <StepView state={state} dispatch={dispatch} />
       </main>
     </div>
