a0087e5f5910fe2b9397548cd31025b947a81dc1
jordansbenjamin <jordansbdev@gmail.com>
2026-05-24 21:31:11 +1000

    feat: continued styling PasteStep view

--- src/components/Paste/PasteStep.tsx
+++ src/components/Paste/PasteStep.tsx
@@ -1,7 +1,20 @@
 import type { StepProps } from "../../types/shared";
+import styles from "./PasteStep.module.css"
 
 export default function PasteStep({state, dispatch}: StepProps) {
   return (
-    <div>PasteStep</div>
+    <div className={styles.pasteStepView}>
+      <div>PasteStep</div>
+      <div>
+        <textarea/>
+        <div>
+          <button>Clear</button>
+          <button>Load example</button>
+        </div>
+        <div>
+          <button>Parse entries</button>
+        </div>
+      </div>
+    </div>
   )
 }
